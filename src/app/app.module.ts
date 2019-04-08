369daeea2492963eec705564ea3b40cb5442925b
mshtang <mshtang@outlook.com>
2019-04-08 20:38:58 +0200

    Angular router basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,11 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -24,6 +29,9 @@ import { FooterComponent } from './footer/footer.component';
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +41,8 @@ import { FooterComponent } from './footer/footer.component';
     MatToolbarModule,
     MatCardModule,
     MatButtonModule,
-    MatListModule
+    MatListModule,
+    AppRoutingModule
   ],
   providers: [
     DishService,
